8a0b6966fd2b0284e90697deb3f191c8e4b6aa36
Robinson Raju <robinson.raju@gmail.com>
2014-07-09 16:40:53 -0700

    project set up

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ clojure-webapp
 ==============
 
 Sample Web application using Clojure
+
---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject clojure-webapp "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+
+  :url "http://example.com/FIXME"
+
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+		[ring "1.3.0"]]
+
+  :plugins [[lein-ring "0.8.11"]]
+
+  :ring (:handler clojure-webapp.core/example-handler
+         :init clojure-webapp.core/on-init
+         :destroy clojure-webapp.core/on-destroy))
