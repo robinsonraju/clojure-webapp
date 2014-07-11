285352cd87a7dfe4c206dab16029b709de0c427a
Robinson Raju <robinson.raju@gmail.com>
2014-07-10 21:48:42 -0700

    generating html content

---
+++ src/clojure_webapp/html.clj
@@ -0,0 +1,8 @@
+(ns clojure-webapp.html)
+
+(defn emit [form]
+  (if (vector? form)
+    (str "<" (name (first form)) ">"
+         (apply str (map emit (rest form)))
+         "</" (name (first form)) ">")
+    (str form)))
